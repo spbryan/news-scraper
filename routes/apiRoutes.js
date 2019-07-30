f2e64e4a3c776cd401a06600c682d5fd15e22aaa
spbryan <seanbryan@comcast.com>
2019-07-29 20:40:39 -0400

    change to add comment id to array rather than single value

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -65,7 +65,7 @@ module.exports = function (app) {
     app.post("/article/:id", function (req, res) {
         db.Comment.create(req.body)
             .then(function (dbComment) {
-                return db.Article.findOneAndUpdate({ _id: req.params.id }, { comment: dbComment._id }, { new: true });
+                return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { comment: dbComment._id } }, { new: true });
             })
             .then(function (dbArticle) {
                 res.render("comment", {
